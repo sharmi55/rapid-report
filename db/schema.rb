a7a2d886cf03923e5d88c604341901a1f69a52ea
Sharmi <rai.sharmistha@gmail.com>
2016-05-04 16:11:39 +0200

    add association between User and Report

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160504131607) do
+ActiveRecord::Schema.define(version: 20160504140935) do
 
   create_table "reports", force: :cascade do |t|
     t.string   "first_name"
@@ -19,7 +19,10 @@ ActiveRecord::Schema.define(version: 20160504131607) do
     t.text     "summary"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "reports", ["user_id"], name: "index_reports_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
