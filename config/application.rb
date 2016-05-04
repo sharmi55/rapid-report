7c1eb293e189ea740f6fa81710afd5c644e24128
Sharmi <rai.sharmistha@gmail.com>
2016-05-04 15:14:44 +0200

    add devise gem and install devise views

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module RapidReport
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.initialize_on_precompile = false
+    #If deploying to Heroku
   end
 end
