05d902c5012080838e9f1a73d0c3fe627ab2c5af
Sharmi <rai.sharmistha@gmail.com>
2016-05-07 13:41:39 +0200

    see profile pic in nav bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,6 @@
 module ApplicationHelper
-  def gravatar_for(user, options = { size: 80})
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    size = options[:size]
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.email, class: "img-circle")
+  def avatar_url(user)
+  gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+  "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
   end
 end
