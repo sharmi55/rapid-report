03056602e0ac6f6c553e0c9e65af2ac92ebad47a
Robb Shecter <robb@weblaws.org>
2016-05-17 22:19:04 -0700

    Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,8 +13,8 @@ sharmi = User.create!(email: 'sharmi@somewhere.com', password: 'password')
 50.times do
   Report.create!(
     birthday: (rand(10)+1).years.ago,
-    first_name: "Joe",
-    last_name: "Smith",
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
     user: sharmi
     )
 end
