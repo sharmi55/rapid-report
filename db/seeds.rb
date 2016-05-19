18e082371d54f618be1398ab379c552bc11840fe
Sharmi <rai.sharmistha@gmail.com>
2016-05-19 15:13:31 +0700

    change seeds and color of bars in chart

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,13 @@ sharmi = User.create!(email: 'sharmi@somewhere.com', password: 'password')
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     user: sharmi,
-    grades: 'Math 100, English 89, Geography 93'
+    grades: 'Vietnamese 94, Math 100, English 89, History 93, Biology 76',
+    line1: Faker::Address.street_address,
+    province: Faker::Address.state,
+    phone1: Faker::PhoneNumber.phone_number,
+    relation1: Faker::Name.name,
+    phone2: Faker::PhoneNumber.phone_number,
+    relation2: Faker::Name.name,
+    summary: Faker::Hipster.paragraphs
     )
 end
